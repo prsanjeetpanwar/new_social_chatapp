a216bcde37518f57728c80ae19cd09b331bafcb5
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-10-05 09:42:41 +0530

    new

---
+++ app/(site)/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Authform from "./Components/Authform";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="logo"
+          height={48}
+          width={48}
+          src="/images/logo1.png" // Path is relative to the "public" directory
+          className="mx-auto w-auto"
+        />
+        <h2 className="mt-6
+        text-center
+        text-3xl
+        font-bold
+        tracking-tight
+        text-gray-900
+        ">
+Sign in to your account
+        </h2>
+      </div>
+      <Authform/>
+    </div>
+  );
+}
